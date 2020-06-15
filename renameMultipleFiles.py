dc3b7c34a88765c8b47a488811e4abdce4244c5a
michal037 <michalkozakiewicz3@gmail.com>
2020-06-15 10:32:55 +0200

    Update renameMultipleFiles.py

--- renameMultipleFiles.py
+++ renameMultipleFiles.py
@@ -4,18 +4,51 @@
 import os, platform # rename()
 import argparse     # main()
 
-def rename(path, newName='', newExtension=None):
-	"""
-		This function renames files in the directory.
+def rename(path, new_name='', new_extension=None):
+	'''
+		This function renames multiple files in the directory.
 
 		Input:
-		  path         - directory where rename files
-		  newName      - new filename to set (optional)
-		  newExtension - new extension to set without dot (optional)
+		  path          - directory where rename files
+		  new_name      - new file name to set
+		  new_extension - new extension to set
 		
 		Return:
 		  None
-	"""
+	'''
+
+	# check types
+	if type(path) != str:
+		raise TypeError('path have to be string')
+
+	if type(new_name) != str:
+		raise TypeError('new_name have to be string')
+
+	if type(new_extension) not in [type(None), type('')]:
+		raise TypeError('new_extension have to be string or None')
+
+	# check values
+	if not os.path.isdir(path):
+		raise Exception('directory given in path does not exist')
+
+	# forbidden characters:    \ / : * ? " < > |
+	FORBIDDEN_CHARACTERS = '\x5C\x2F\x3A\x2A\x3F\x22\x3C\x3E\x7C'
+
+	for char in new_name:
+		if char in FORBIDDEN_CHARACTERS:
+			raise ValueError(
+				'the file name cannot contain any of the following ' +
+				'characters: ' + FORBIDDEN_CHARACTERS)
+
+	if type(new_extension) == str:
+		for char in new_extension:
+			if char in FORBIDDEN_CHARACTERS:
+				raise ValueError(
+					'the file extension cannot contain any of the following ' +
+					'characters: ' + FORBIDDEN_CHARACTERS)
+
+		# remove dots from beginning and ending
+		new_extension = new_extension.strip('.')
 
 	# add slash / backslash at the end of path
 	if platform.system() == 'Windows':
@@ -25,41 +58,44 @@ def rename(path, newName='', newExtension=None):
 		if path[-1] != '/':
 			path += '/'
 
-	count = 0 # count this way because enumerate includes directories etc.
+	count = 1 # count this way because enumerate includes directories etc.
 
-	for fileName in os.listdir(path):
+	for file_name in os.listdir(path):
 		# skip other than file
-		if not os.path.isfile(path + fileName):
+		if not os.path.isfile(path + file_name):
 			continue
 
-		dst = newName + str(count)
+		dst = new_name + str(count)
 
-		# use 'newExtension' if not empty, else use 'fileName' extension
-		if newExtension != None:
-			dst += '.' + newExtension
-		else:
-			dst += os.path.splitext(fileName)[1]
+		# without specified extension try to use default
+		# else for non empty new_extension set this extension
+		if new_extension == None:
+			dst += os.path.splitext(file_name)[1]
+		elif new_extension != '':
+			dst += '.' + new_extension
 
 		dst = path + dst
-		src = path + fileName
+		src = path + file_name
 
 		os.rename(src, dst)
 		count += 1
 
 
 def main():
-	parser = argparse.ArgumentParser(description='This script renames files in the directory.')
+	parser = argparse.ArgumentParser(
+		description='This script renames multiple files in the directory.')
 	
 	parser.add_argument('path', help='directory where rename files')
-	parser.add_argument('-n', '--name', default='', help='new filename to set')
-	parser.add_argument('-e', '--extension', default=None, help='new extension to set (without dot)')
+	parser.add_argument('-n', '--name', default='', help='new file name to set')
+	parser.add_argument('-e', '--extension', default=None,
+		help='new extension to set')
 
 	args = parser.parse_args()
 
-	if not os.path.isdir(args.path):
-		parser.error('directory does not exist')
-
-	rename(args.path, args.name, args.extension)
+	try:
+		rename(args.path, args.name, args.extension)
+	except Exception as e:
+		print('Error: {}'.format(e))
 	
 
 if __name__ == '__main__':
